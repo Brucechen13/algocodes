37cd1d1799612c977997bc3a0ad41a151e8a44fc
Brucechen13 <chenchi_94@163.com>
2017-06-11 20:28:58 +0800

    add codes

--- codes/src/algo/array/ContainsDuplicate.java
+++ codes/src/algo/array/ContainsDuplicate.java
@@ -17,4 +17,13 @@ public class ContainsDuplicate {
         }
         return false;
     }
+    //Contains Duplicate II
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<Integer>();
+        for(int i = 0; i < nums.length; i++){
+            if(i > k) set.remove(nums[i-k-1]);
+            if(!set.add(nums[i])) return true;
+        }
+        return false;
+    }
 }
