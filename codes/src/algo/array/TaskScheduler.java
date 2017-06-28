057658674b23761172cd63e088fd84d3de5cc858
Brucechen13 <chenchi_94@163.com>
2017-06-28 20:42:12 +0800

    add codes

---
+++ codes/src/algo/array/TaskScheduler.java
@@ -0,0 +1,13 @@
+package algo.array;
+
+/**
+ * Created by chenc on 2017/6/28.
+ */
+public class TaskScheduler {
+    public int leastInterval(char[] tasks, int n) {
+        int sum = 0;
+
+
+        return sum;
+    }
+}
