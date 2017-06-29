2c5a60ebfcb5184976ee6f720d6511dd024f8406
Brucechen13 <chenchi_94@163.com>
2017-06-29 16:37:25 +0800

    add codes

--- codes/src/algo/array/TaskScheduler.java
+++ codes/src/algo/array/TaskScheduler.java
@@ -1,13 +1,20 @@
 package algo.array;
 
+import java.util.Arrays;
+
 /**
  * Created by chenc on 2017/6/28.
  */
 public class TaskScheduler {
     public int leastInterval(char[] tasks, int n) {
-        int sum = 0;
+        int[] c = new int[26];
+        for(char t : tasks){
+            c[t - 'A']++;
+        }
+        Arrays.sort(c);
+        int i = 25;
+        while(i >= 0 && c[i] == c[25]) i--;
 
-
-        return sum;
+        return Math.max(tasks.length, (c[25] - 1) * (n + 1) + 25 - i);
     }
 }
