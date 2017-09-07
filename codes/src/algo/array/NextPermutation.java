9f6d664a0d280afaa41241f7431225535827fc5e
Brucechen13 <chenchi_94@163.com>
2017-09-07 20:58:26 +0800

    next perm algo

--- codes/src/algo/array/NextPermutation.java
+++ codes/src/algo/array/NextPermutation.java
@@ -4,27 +4,45 @@ package algo.array;
  * Created by chenc on 2017/8/7.
  */
 public class NextPermutation {
-    public void nextPermutation(int[] nums) {
-        int index = nums.length-1;
-        while (index > 0 && nums[index-1] > nums[index]){
+    public void nextPermutation(int[] num) {
+        int n=num.length;
+        if(n<2)
+            return;
+        int index=n-1;
+        while(index>0){
+            if(num[index-1]<num[index])
+                break;
             index--;
         }
-        reverse(nums, Math.max(0, index-1), nums.length-1);
-    }
-
-    public void reverse(int[]nums, int start, int end){
-        while (start < end){
-            int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
-            start++;end--;
+        if(index==0){
+            reverseSort(num,0,n-1);
+            return;
+        }
+        else{
+            int val=num[index-1];
+            int j=n-1;
+            while(j>=index){
+                if(num[j]>val)
+                    break;
+                j--;
+            }
+            swap(num,j,index-1);
+            reverseSort(num,index,n-1);
+            return;
         }
     }
+
+    public void swap(int[] num, int i, int j){
+        int temp=0;
+        temp=num[i];
+        num[i]=num[j];
+        num[j]=temp;
+    }
+
+    public void reverseSort(int[] num, int start, int end){
+        if(start>end)
+            return;
+        for(int i=start;i<=(end+start)/2;i++)
+            swap(num,i,start+end-i);
+    }
 }
-
-/1-2-3-4 1-2-4-3
-/4-3-2-1
-/4-1-3-2 4-2-1-3
-
- 3 5 4 2 1
- 4 1 2 3 5
